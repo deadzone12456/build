f2784413da93938639252d9cb9a24bde9d65cd2e
kudo <deadzone12456@gmail.com>
2024-11-23 19:28:34 +0530

    Update build.sh

--- build.sh
+++ build.sh
@@ -24,7 +24,7 @@ rm -rf device/qcom/sepolicy_vndr/legacy-um
 
 # Device
 git clone https://github.com/shinichi-c/android_device_oneplus_fajita_15 --depth=1 -b pixelage_4.19 device/oneplus/fajita
-git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_15 --depth=1 -b pixelage_T device/oneplus/sdm845-common
+git clone https://github.com/shinichi-c/android_device_oneplus_sdm845-common_15 --depth=1 -b upstream-4.19 device/oneplus/sdm845-common
 
 # Vendor
 git clone https://github.com/TheMuppets/proprietary_vendor_oneplus_fajita --depth=1 -b lineage-22.0 vendor/oneplus/fajita
@@ -37,9 +37,9 @@ git clone https://github.com/Terminator-J/crdroid_kernel_oneplus_sdm845 --depth=
 git clone https://github.com/shinichi-c/android_hardware_oneplus --depth=1 -b pixelage hardware/oneplus
 
 # Hals
-git clone https://github.com/shinichi-c/media --depth=1 -b 14.0-4.19 hardware/qcom-caf/sdm845/media
+git clone https://github.com/shinichi-c/media --depth=1 -b upstream hardware/qcom-caf/sdm845/media
 git clone https://github.com/shinichi-c/audio --depth=1 -b pixelage hardware/qcom-caf/sdm845/audio
-git clone https://github.com/shinichi-c/display --depth=1 -b Lineage-22 hardware/qcom-caf/sdm845/display
+git clone https://github.com/shinichi-c/display --depth=1 -b upstream hardware/qcom-caf/sdm845/display
 
 # Sepolicy
 git clone https://github.com/shinichi-c/android_device_pixelage_sepolicy --depth=1 device/lineage/sepolicy
